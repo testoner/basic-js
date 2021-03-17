c459b7a0e82d46b6d4397f2a496b1fff7a0cb5a6
testoner <ekgeretoker@gmail.com>
2021-03-17 11:53:17 +0300

    feat: update some js files

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  //let sampleActivity = "3";
+  //if (typeof (+sampleActivity == "undefined")) return false;
+  if (typeof (sampleActivity) != "string") return false;
+  if (typeof (+sampleActivity) == "undefined") console.log(false);
+  sampleActivity = +sampleActivity;
+  let ln = Math.log(MODERN_ACTIVITY / sampleActivity);
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let res = ln / k;
+  if ((isNaN(res)) || (res < 0) || (res == Infinity)) return false;
+  return Math.ceil(res);
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -2,10 +2,14 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
   let res = 0;
+  let obs = [];
+  //let matrix = [['##', 'dd', '00'],['^^', '..', 'ss'],['AA', 'dd', 'Oo'],];
   for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; i < matrix[i].length; j++) {
-      if (matrix[i][j] == '^^') res++;
+    obs = matrix[i];
+    for (let j = 0; j < obs.length; j++) {
+      if (obs[j] == '^^') res += 1;
     }
   }
   return res;
+  //console.log(res);
 };
