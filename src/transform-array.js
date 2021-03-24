0c601492e6ca1e64101c3ea81c3f77785e755cd1
testoner <ekgeretoker@gmail.com>
2021-03-24 23:00:13 +0300

    feat: add new solution transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!(arr instanceof Array)) throw Error;
+  let res = [];
+  let l = arr.length;
+  for (let i = 0; i < l; i++) {
+    if (arr[i] == "--discard-next" && i != l - 1) {
+      if (arr[i + 2] == "--discard-prev" || arr[i + 2] == "--double-prev") {
+        i += 2;
+      } else {
+        if (i != l && i != l - 1) {
+          i += 1;
+        }
+      }
+    } else
+      if (arr[i + 1] == "--discard-prev") {
+        i += 1;
+
+      } else
+        if (arr[i] == "--double-next" && i != l - 1) {
+          res.push(arr[i + 1]);
+
+        } else
+          if (arr[i + 1] == "--double-prev") {
+            res.push(arr[i]);
+            res.push(arr[i]);
+            i += 1;
+          } else {
+            if (arr[i] != "--double-prev" && arr[i] != "--discard-prev" && arr[i] != "--double-next" && arr[i] != "--discard-next") res.push(arr[i]);
+          }
+  }
+  return res;
 };
