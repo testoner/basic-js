1a198565f3fc421817aa44c1161fe49a2df547bf
testoner <ekgeretoker@gmail.com>
2021-03-29 17:21:21 +0300

    feat: update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,46 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(rev) {
+    this.rev = rev === false ? "reverse" : "direct";
+  }
+  encrypt(s, l) {
+    if (!s || !l) throw Error();
+    const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let res = "";
+    s = s.toUpperCase();
+    l = l.toUpperCase();
+    let i = 0;
+    for (let j = 0; j < s.length; j++) {
+      if (abc.indexOf(s[j]) < 0) {
+        res += s[j];
+        i++;
+      } else {
+        const msgIndex = abc.indexOf(s[j]);
+        const keyIndex = abc.indexOf(l[(j - i) % l.length]);
+        res = res + abc.charAt((msgIndex + keyIndex) % abc.length);
+      }
+    }
+    return this.rev === "reverse" ? res.split("").reverse().join("") : res;
+  }
+  decrypt(s, l) {
+    if (!s || !l) throw Error();
+    const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let res = "";
+    s = s.toUpperCase();
+    l = l.toUpperCase();
+    let i = 0;
+    for (let j = 0; j < s.length; j++) {
+      if (abc.indexOf(s[j]) < 0) {
+        res += s[j];
+        i++;
+      } else {
+        const msgIndex = abc.indexOf(s[j]);
+        const keyIndex = abc.indexOf(l[(j - i) % l.length]);
+        res = res + abc.charAt((msgIndex + abc.length - keyIndex) % abc.length);
+      }
+    }
+    return this.rev === "reverse" ? res.split("").reverse().join("") : res;
   }
 }
 
