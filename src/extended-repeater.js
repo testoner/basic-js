64d1f1293cfcd1b4432cb86aabc62e1600ba7e2a
testoner <ekgeretoker@gmail.com>
2021-03-29 14:25:58 +0300

    feat: update vigenere-cipher.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let { repeatTimes = 1, separator = '+', addition = "", additionRepeatTimes = 1, additionSeparator = '|' } = options;
+  let res = '';
+  for (let i = 0; i < repeatTimes; i++) {
+    res += str;
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      res += addition;
+      if (j + 1 < additionRepeatTimes) res += additionSeparator;
+    }
+    if (i + 1 < repeatTimes) {
+      res += separator;
+    }
+  }
+  return (res);
 };
-  
